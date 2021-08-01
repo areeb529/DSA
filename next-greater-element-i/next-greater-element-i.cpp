deb4c20aa5f733fc09ec2b577c9119319408279b
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-08-01 11:09:44 +0530

    Time: 4 ms (95.13%), Space: 9 MB (31.53%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans(nums1.size(),-1);
-        for(int i=0;i<nums1.size();i++){
-            bool flag=false;
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    flag=true;
-                    continue;
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<int> ans(n,0);
+        stack<int> s;
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[nums1[i]]=i;
+        }
+            for(int j=m-1;j>=0;j--){
+                if(s.empty()){
+                    s.push(nums2[j]);
+                    if(mp.count(nums2[j])==1){
+                        ans[mp[nums2[j]]]=-1;
+                    }
                 }
-                if(flag){
-                    if(nums2[j]>nums1[i]){
-                        ans[i]=nums2[j];
-                        break;
+                else if(nums2[j]<s.top()){
+                    if(mp.count(nums2[j])==1){
+                        ans[mp[nums2[j]]]=s.top();
+                    }
+                    s.push(nums2[j]);
+                }
+                else if(nums2[j]>s.top()){
+                    while(!s.empty()&&nums2[j]>s.top()){
+                        s.pop();
+                    }
+                    if(s.empty()){
+                        if(mp.count(nums2[j])==1){
+                            ans[mp[nums2[j]]]=-1;
+                        }
+                        s.push(nums2[j]);
+                    }
+                    else{
+                        if(mp.count(nums2[j])==1){
+                            ans[mp[nums2[j]]]=s.top();
+                        }
+                        s.push(nums2[j]);
                     }
                 }
             }
-        }
         return ans;
     }
 };
