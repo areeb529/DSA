69d855dc9ccb56054882b991807a6038ac527426
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-08-02 22:34:34 +0530

    Time: 184 ms (93.24%), Space: 84.1 MB (91.79%) - LeetHub

--- online-stock-span/online-stock-span.cpp
+++ online-stock-span/online-stock-span.cpp
@@ -1,37 +1,18 @@
 class StockSpanner {
 public:
-    vector<int> v;
-    stack<int> s;
-    //unordered_map<int> mp;
+    stack<pair<int,int>> s;
     StockSpanner() {
         
     }
     
     int next(int price) {
-        v.push_back(price);
-        if(s.empty()){
-            s.push(v.size()-1);
-            return 1;
+        int res=1;
+        while(!s.empty()&&s.top().first<=price){
+            res+=s.top().second;
+            s.pop();
         }
-        else if(price<v[s.top()]){
-            s.push(v.size()-1);
-            return 1;
-        }
-        else if(price>=v[s.top()]){
-            while(!s.empty()&&price>=v[s.top()]){
-                s.pop();
-            }
-            if(s.empty()){
-                s.push(v.size()-1);
-                return v.size();
-            }
-            else{
-                int temp=s.top();
-                s.push(v.size()-1);
-                return v.size()-1-temp;
-            }
-        }
-        return 1;
+        s.push({price,res});
+        return res;
     }
 };
 
