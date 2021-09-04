8c88bc4b7b203fa3f38de2406acdfe2b7e45b20f
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-09-05 01:39:51 +0530

    Time: 44 ms (58.88%), Space: 17 MB (43.19%) - LeetHub

---
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -0,0 +1,22 @@
+ bool compare(const vector<int> &a,const vector<int>&b){
+        return a[1]>b[1];
+    }
+class Solution {
+public:
+   
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(),boxTypes.end(),compare);
+        int ans=0;
+        for(auto i:boxTypes){
+            if(truckSize>=i[0]){
+                truckSize-=i[0];
+                ans+=i[0]*i[1];
+            }
+            else{
+                ans+=i[1]*truckSize;
+                break;
+            }
+        }
+        return ans;
+    }
+};
