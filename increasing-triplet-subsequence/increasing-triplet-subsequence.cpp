bd6a02a5b703c2244e5903d2e06c0b4787c76acc
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-09-07 20:44:24 +0530

    Time: 68 ms (46.74%), Space: 61.5 MB (69.83%) - LeetHub

--- increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int sminM=INT_MAX;
-        int minM=INT_MAX;
-        int ct=0;
+        // int sminM=INT_MAX;
+        // int minM=INT_MAX;
+        // int ct=0;
+        // for(auto i:nums){
+        //     if(i>minM&&sminM!=i){
+        //         if(ct==1){
+        //           if(i>sminM){
+        //               ct++;
+        //               return true;
+        //           }
+        //         else{
+        //             sminM=i;
+        //         }
+        //         }
+        //         else{
+        //             sminM=i;
+        //             ct++;
+        //         }
+        //     }
+        //     minM=min(i,minM);
+        // }
+        // return false;
+        int c1=INT_MAX;
+        int c2=INT_MAX;
         for(auto i:nums){
-            if(i>minM&&sminM!=i){
-                if(ct==1){
-                  if(i>sminM){
-                      ct++;
-                      return true;
-                  }
-                else{
-                    sminM=i;
-                }
-                }
-                else{
-                    sminM=i;
-                    ct++;
-                }
+            if(i<c1){
+                c1=i;
             }
-            minM=min(i,minM);
+            else if(i>c1&&i<c2){
+                c2=i;
+            }
+            else if(c2<i){
+                return true;
+            }
         }
         return false;
-        
     }
 };
