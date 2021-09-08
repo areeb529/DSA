6c9470434ab6a817d07d0fa8ec6838acc8c8577c
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-09-08 11:16:40 +0530

    Time: 8 ms (66.37%), Space: 9.9 MB (51.32%) - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     unordered_map<int,int> mp;
-    int helper(vector<int> &v,int i, vector<int> &dp){
-        if(i>=v.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-        int a=0,b=0;
-        if(i+1>=v.size()||v[i+1]-v[i]>1){
-        a=v[i]*mp[v[i]]+helper(v,i+1,dp);    
-        }
-        else{
-        a=v[i]*mp[v[i]]+helper(v,i+2,dp);
-        b=helper(v,i+1,dp);
-        }
-        return dp[i]=max(a,b);
-    }
     int deleteAndEarn(vector<int>& nums) {
         vector<int> v;
         for(auto i:nums){
             if(mp.count(i)==0){
-                mp[i]=1;
+                mp[i]=i;
                 v.push_back(i);
             }
-            else mp[i]++;
+            else mp[i]+=i;
         }
         int n=v.size();
         sort(v.begin(),v.end());
-        vector<int> dp(n+1,-1);
-        return helper(v,0,dp);
+        vector<int> dp(n+2);
+        dp[n+1]=0;
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            if(i+1>=n||v[i+1]-v[i]>1)dp[i]=mp[v[i]]+dp[i+1];
+            else dp[i]=max(mp[v[i]]+dp[i+2],dp[i+1]);
+        }
+        return dp[0];
     }
 };
