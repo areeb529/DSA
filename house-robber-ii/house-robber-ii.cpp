15a0c8306565058102540e66aff66e5ee21f10b7
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-09-09 12:37:12 +0530

    Time: 0 ms (100.00%), Space: 8 MB (37.35%) - LeetHub

--- house-robber-ii/house-robber-ii.cpp
+++ house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int helper(vector<int>& nums,int i,vector<int>&dp){
-        if(i>=nums.size()){
+    int helper(vector<int> &nums,int i,int n){
+        if(i==n||n==0){
             return 0;
         }
-        if(dp[i]!=-1)return dp[i];
-        int opt1=nums[i]+helper(nums,i+2,dp);
-        int opt2=helper(nums,i+1,dp);
-        return dp[i]=max(opt1,opt2);
+        vector<int> dp(n+2);
+        dp[n+1]=0;
+        dp[n]=0;
+        for(int j=n-1;j>=i;j--){
+            dp[j]=max(nums[j]+dp[j+2],dp[j+1]);
+        }
+        return dp[i];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
         int i=0;
-        vector<int> dp2(n,-1);
-        int opt2=helper(nums,i+1,dp2);
+        int opt2=helper(nums,i+1,n);
         nums.pop_back();
-        vector<int> dp1(n+1,-1);
-        int opt1=nums[i]+helper(nums,i+2,dp1);
+        int opt1=nums[i]+helper(nums,i+2,n-1);
         return max(opt1,opt2);
     }
 };
