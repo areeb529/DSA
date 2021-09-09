52bed6faa1fdc3ec53c19216ea699280ffbc6573
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-09-09 14:12:00 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (96.66%) - LeetHub

--- house-robber-ii/house-robber-ii.cpp
+++ house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int helper(vector<int> &nums,int i,int n){
-        if(i==n||n==0){
+        if(i>n){
             return 0;
         }
-        vector<int> dp(n+2);
-        dp[n+1]=0;
-        dp[n]=0;
-        for(int j=n-1;j>=i;j--){
-            dp[j]=max(nums[j]+dp[j+2],dp[j+1]);
+        int b=0;
+        int c=0;
+        for(int j=n;j>=i;j--){
+            int a=max(nums[j]+b,c);
+            b=c;
+            c=a;
         }
-        return dp[i];
+        return c;
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
         int i=0;
-        int opt2=helper(nums,i+1,n);
-        nums.pop_back();
-        int opt1=nums[i]+helper(nums,i+2,n-1);
+        int opt1=helper(nums,i,n-2);
+        int opt2=helper(nums,i+1,n-1);
+        
         return max(opt1,opt2);
     }
 };
