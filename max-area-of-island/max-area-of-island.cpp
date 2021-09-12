3a85550cb840a11a36c0d717f8c281f8183f3b6e
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-09-12 17:34:38 +0530

    Time: 12 ms (96.39%), Space: 26.7 MB (19.03%) - LeetHub

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int m,int n,int i,int j,vector<vector<bool>>& visited,int &ct){
-        if(i<0||i>=m||j<0||j>=n||visited[i][j]||!grid[i][j])return;
-        visited[i][j]=true;
-        ct++;
-        dfs(grid,m,n,i-1,j,visited,ct);
-        dfs(grid,m,n,i,j-1,visited,ct);
-        dfs(grid,m,n,i+1,j,visited,ct);
-        dfs(grid,m,n,i,j+1,visited,ct);
-    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
-        int res=0;
+        int area=0,offsets[]={0,1,0,-1,0};
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]&&!visited[i][j]){
-                  int ct=0;
-                    dfs(grid,m,n,i,j,visited,ct);
-                    res=max(res,ct);
+                if(grid[i][j]){
+                    int a=1;
+                    queue<pair<int,int>> todo;
+                    todo.push({i,j});
+                    grid[i][j]=0;
+                    while(!todo.empty()){
+                    pair<int,int> f=todo.front();
+                    todo.pop();
+                    for(int k=0;k<4;k++){
+                        int r=f.first+offsets[k];
+                        int c=f.second+offsets[k+1];
+                        if(r>=0&&r<m&&c>=0&&c<n&&grid[r][c]){
+                            todo.push({r,c});
+                            a++;
+                            grid[r][c]=0;
+                        }
+                    }
+                        
+                  }
+                    area=max(area,a);
                 }
             }
         }
-        return res;
+        return area;
     }
 };
