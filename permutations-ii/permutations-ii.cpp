17a3b6ec90484a077bc6adaa11ac0e303e422d83
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-10-01 00:01:10 +0530

    Time: 12 ms (48.32%), Space: 11.4 MB (20.96%) - LeetHub

---
+++ permutations-ii/permutations-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     vector<vector<int>> ans;
+    void helper(vector<int>& nums,int i,int n){
+        if(i==n){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> st;
+        for(int j=i;j<n;j++){
+            if(st.count(nums[j])==0)st.insert(nums[j]);
+            else continue;
+            swap(nums[i],nums[j]);
+            helper(nums,i+1,n);
+            swap(nums[i],nums[j]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n=nums.size();
+        helper(nums,0,n);
+        return ans;
+    }
+};
