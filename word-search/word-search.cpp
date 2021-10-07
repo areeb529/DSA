15bcba2096c3c20ccdb9263a639f3aff6cd3be94
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-10-08 01:34:50 +0530

    Time: 196 ms (89.18%), Space: 7.4 MB (53.35%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-bool dfs(vector<vector<char>>& board,int m,int n,int i,int j, string &word,int k,vector<vector<bool>>& visited){
+bool dfs(vector<vector<char>>& board,int m,int n,int i,int j, string &word,int k){
         if(k==word.size()){
             return true;
         }
-        if(i<0||i>=m||j<0||j>=n||board[i][j]!=word[k]||visited[i][j])return false;
-        visited[i][j]=true;
-        bool opt1=dfs(board,m,n,i-1,j,word,k+1,visited);
-        if(opt1)return true;
-        bool opt2=dfs(board,m,n,i,j-1,word,k+1,visited);
-        if(opt2)return true;
-        bool opt3=dfs(board,m,n,i+1,j,word,k+1,visited);
-        if(opt3)return true;
-        bool opt4=dfs(board,m,n,i,j+1,word,k+1,visited);
-        if(opt4)return true;;
-        visited[i][j]=false;
+        if(i<0||i>=m||j<0||j>=n||board[i][j]=='*'||board[i][j]!=word[k])return false;
+        char temp=board[i][j];
+        board[i][j]='*';
+        bool ans=dfs(board,m,n,i-1,j,word,k+1)||
+                 dfs(board,m,n,i,j-1,word,k+1)||
+                 dfs(board,m,n,i+1,j,word,k+1)||
+                 dfs(board,m,n,i,j+1,word,k+1);
+        if(ans)return true;;
+        board[i][j]=temp;
         return false;
         
     }
@@ -22,12 +20,11 @@ bool dfs(vector<vector<char>>& board,int m,int n,int i,int j, string &word,int k
         int m=board.size();
         int n=board[0].size();
         int i=0,j=0,k=0;
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
          for(int i=0;i<m;i++){
              for(int j=0;j<n;j++){
                  bool ans=false;
-                 if(board[i][j]==word[0])
-                 ans=dfs(board,m,n,i,j,word,k,visited);
+                 if(board[i][j]==word[k])
+                 ans=dfs(board,m,n,i,j,word,k);
                  if(ans) return true;
              }
          }
