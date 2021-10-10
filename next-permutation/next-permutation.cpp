fb7944359473cf27157e3d23e8bed333f8e5b28e
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-10-10 19:00:55 +0530

    Time: 4 ms (76.26%), Space: 11.9 MB (93.55%) - LeetHub

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -6,10 +6,10 @@ public:
             int mid=s+(e-s)/2;
             if(nums[mid]>key){
                 ans=mid;
-                s=mid+1;
+                e=mid-1;
             }
             else{
-                e=mid-1;
+                s=mid+1;
             }
         }
         return ans;
@@ -20,9 +20,9 @@ public:
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 f=true;
+                reverse(nums.begin()+i+1,nums.end());
                 int idx=bs(nums,i+1,n-1,nums[i]);
                 swap(nums[i],nums[idx]);
-                sort(nums.begin()+i+1,nums.end());
                 break;
             }
         }
