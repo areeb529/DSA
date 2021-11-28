53e13be557b0dfa76dbbe1feb03af2bdaed7b89f
areeb529 <69076491+areeb529@users.noreply.github.com>
2021-11-28 20:01:18 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (75.79%) - LeetHub

--- interleaving-string/interleaving-string.cpp
+++ interleaving-string/interleaving-string.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
-    bool helper(string &s1,int i,string &s2,int j,string &s3,vector<vector<int>> &dp){
-        if(dp[i][j]!=-1)return dp[i][j];
-        int k=i+j;
-        if(k==s3.size()){
-            return dp[i][j]=1;
-        }
-        else if(i<s1.size()&&s1[i]==s3[k]&&j<s2.size()&&s2[j]==s3[k]){
-            return dp[i][j]=helper(s1,i+1,s2,j,s3,dp)||helper(s1,i,s2,j+1,s3,dp);
-        }
-        else if(i<s1.size()&&s1[i]==s3[k]){
-            return dp[i][j]=helper(s1,i+1,s2,j,s3,dp);
-        }
-        else if(j<s2.size()&&s2[j]==s3[k]){
-            return dp[i][j]=helper(s1,i,s2,j+1,s3,dp);
-        }
-        else{
-            return dp[i][j]=0;
-        }
-    }
+    // bool helper(string &s1,int i,string &s2,int j,string &s3,vector<vector<int>> &dp){
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int k=i+j;
+    //     if(k==s3.size()){
+    //         return dp[i][j]=1;
+    //     }
+    //     else if(i<s1.size()&&s1[i]==s3[k]&&j<s2.size()&&s2[j]==s3[k]){
+    //         return dp[i][j]=helper(s1,i+1,s2,j,s3,dp)||helper(s1,i,s2,j+1,s3,dp);
+    //     }
+    //     else if(i<s1.size()&&s1[i]==s3[k]){
+    //         return dp[i][j]=helper(s1,i+1,s2,j,s3,dp);
+    //     }
+    //     else if(j<s2.size()&&s2[j]==s3[k]){
+    //         return dp[i][j]=helper(s1,i,s2,j+1,s3,dp);
+    //     }
+    //     else{
+    //         return dp[i][j]=0;
+    //     }
+    // }
     bool isInterleave(string s1, string s2, string s3) {
+        // int m=s1.size(),n=s2.size();
+        // if(m+n!=s3.size())return false;
+        // vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        // return helper(s1,0,s2,0,s3,dp);
+        
         int m=s1.size(),n=s2.size();
         if(m+n!=s3.size())return false;
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return helper(s1,0,s2,0,s3,dp);
+        vector<int> dp(n+1,0);
+        for(int i=m;i>=0;i--){
+            
+            for(int j=n;j>=0;j--){
+                int k=i+j;
+                if(k==s3.size()){
+                    dp[j]=1;
+                }
+                else if(i<m&&s1[i]==s3[k]&&j<n&&s2[j]==s3[k]){
+                    dp[j]=dp[j]||dp[j+1];
+                }
+                else if(i<m&&s1[i]==s3[k]){
+                    dp[j]=dp[j];
+                }
+                else if(j<n&&s2[j]==s3[k]){
+                    dp[j]=dp[j+1];
+                }
+                else{
+                    dp[j]=0;
+                }
+            }
+        }
+        return dp[0];
     }
 };
