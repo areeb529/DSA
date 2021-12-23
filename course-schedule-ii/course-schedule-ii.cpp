5b5a5f13b76fab9847cbaca4bb6f7cb2701cf826
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2021-12-24 01:43:40 +0530

    Time: 20 ms (75.46%), Space: 14 MB (60.91%) - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    
+    vector<int> ans;
+    bool isCycle(vector<int> adj[],int n,int sv,vector<int> &visited){
+        if(visited[sv]==-1)return false;
+        visited[sv]=-1;
+        for(auto &i:adj[sv]){
+            if(visited[i]==-2)continue;
+            bool f=isCycle(adj,n,i,visited);
+            if(!f)return false;
+        }
+        visited[sv]=-2;
+        ans.push_back(sv);
+        return true;
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
         vector<int> adj[n];
-        vector<int> inDegrees(n,0);
         for(auto &i:prerequisites){
-            adj[i[1]].push_back(i[0]);
-            inDegrees[i[0]]++;
+            adj[i[0]].push_back(i[1]);
         }
-        vector<int> res;
-        
-        queue<int> pN;
+        vector<int> visited(n,0);
         for(int i=0;i<n;i++){
-            if(inDegrees[i]==0)pN.push(i);
-        }
-        int cnt=0;
-        while(!pN.empty()){
-            int node=pN.front();
-            pN.pop();
-            cnt++;
-            res.push_back(node);
-            for(auto &i:adj[node]){
-                inDegrees[i]--;
-                if(inDegrees[i]==0)pN.push(i);
+            if(!visited[i]){
+                bool f=isCycle(adj,n,i,visited);
+                if(!f){
+                    ans={};
+                    return ans;
+                }
             }
         }
-        if(cnt==n)return res;
-        else return {};
-        
+        return ans;
     }
 };
