93c74a53a8514e8385d09f03e02f4e84ead026dd
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-03 01:01:52 +0530

    Time: 64 ms (10.10%), Space: 23 MB (99.44%) - LeetHub

--- pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -3,20 +3,11 @@ public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int n=time.size();
         vector<int> mp(60,0);
-        for(int i=0;i<n;i++){
-            time[i]=time[i]%60;
-            mp[time[i]]++;
+        int res=0;
+        for(auto t:time){
+            res+=mp[(60-t%60)%60];
+            mp[t%60]++;
         }
-        int ans=0;
-        for(int i=0;i<60;i++){
-            if(i==0||i==30){
-                ans=ans+(mp[i]*(mp[i]-1))/2;
-            }
-            else{
-                ans=ans+mp[i]*mp[60-i];
-                mp[60-i]=0;
-            }
-        }
-        return ans;
+        return res;
     }
 };
