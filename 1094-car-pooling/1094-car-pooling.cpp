ece5ab4f90033012dafb2e0ae920dff6af6dea3a
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-07 02:13:48 +0530

    Time: 28 ms (17.97%), Space: 10.2 MB (60.95%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    static bool compare(const vector<int> &a,const vector<int> &b){
-        return a[1]<b[1]||a[1]==b[1]&&a[2]<b[2]||a[1]==b[1]&&a[2]==b[2]&&a[0]<b[0];
-    }
+    
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(),trips.end(),compare);
-        int currCapacity=capacity;
+         vector<int> v(1001,0);
         int n=trips.size();
-
-        vector<int> v(n,1);
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(v[j]){
-                    if(trips[j][2]<=trips[i][1]){
-                        v[j]=0;
-                        currCapacity+=trips[j][0];
-                    }
-                }
-            }
-            currCapacity-=trips[i][0];
+            v[trips[i][1]]-=trips[i][0];
+            v[trips[i][2]]+=trips[i][0];
+        }
+        int currCapacity=capacity;
+        for(int i=0;i<=1000;i++){
+            currCapacity+=v[i];
             if(currCapacity<0)return false;
         }
         return true;
