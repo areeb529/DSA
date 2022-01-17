6648a30539aac960c081c97469c15c13719507ce
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-17 21:55:22 +0530

    Time: 144 ms (76.50%), Space: 47.2 MB (68.16%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph,int sv,vector<int> &visited,vector<int> &ans){
+    bool dfs(vector<vector<int>>& graph,int sv,vector<int> &visited){
+        visited[sv]=1;
         bool f=true;
-        visited[sv]=0;
         for(auto i:graph[sv]){
-            if(visited[i]==-1){
-                f=dfs(graph,i,visited,ans);
+            if(!visited[i]){
+                f=dfs(graph,i,visited);
             }
             else{
-                if(visited[i]!=1)f=false;
+                if(visited[i]==1)f=false;
             }
             if(!f){
-                visited[sv]=2;
+                visited[sv]=1;
                 return false;
             }
         }
-        visited[sv]=1;
-        ans.push_back(sv);
+        visited[sv]=2;
         return true;
+        
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int> visited(n,-1);
+        vector<int> visited(n,0);
         vector<int> ans;
+        bool f=true;
         for(int i=0;i<n;i++){
-            if(visited[i]==-1)
-            dfs(graph,i,visited,ans);
+            if(!visited[i]){
+                f=dfs(graph,i,visited);
+            }
+            else{
+                if(visited[i]==1){
+                    f=false;
+                }
+                else{
+                    f=true;
+                }
+            }
+            if(f)ans.push_back(i);
         }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
