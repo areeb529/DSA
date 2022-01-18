920ab3a5744ecb12eba8a1fe3a0f944f740c1ec0
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-19 01:55:32 +0530

    Time: 12 ms (94.16%), Space: 20.3 MB (20.86%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -4,7 +4,7 @@ public:
         int sz=flowerbed.size();
         for(int i=0;i<sz&&n>0;i++){
             if(flowerbed[i]==0&&(i==0||flowerbed[i-1]==0)&&(i==sz-1||flowerbed[i+1]==0)){
-                flowerbed[i]=1;
+                i++;
                 n--;
             }
         }
