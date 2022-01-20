8955b67b70bbe511c03aa825706007c05b2a44b1
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-21 01:15:30 +0530

    Time: 15 ms (48.85%), Space: 14 MB (8.35%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int cnt[128]={};
-        for(auto c:s)cnt[c]++;
-        sort(s.begin(),s.end(),[&](char a,char b){
-         return cnt[a]>cnt[b]||cnt[a]==cnt[b]&&a<b;
-        });
-        return s;
+        int n=s.size();
+        unordered_map<char,int> mp;
+        for(auto c:s){
+            mp[c]++;
+        }
+        vector<vector<char>> bucket(n+1);
+        for(auto &i:mp){
+            bucket[i.second].push_back(i.first);
+        }
+        string res;
+        for(int i=n;i>=0;i--){
+            for(auto c:bucket[i]){
+                int freq=0;
+                while(freq<i){
+                    res.push_back(c);
+                    freq++;
+                }
+            }
+        }
+        return res;
     }
 };
