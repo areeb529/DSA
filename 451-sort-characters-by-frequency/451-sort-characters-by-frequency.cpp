faff99d38add2fa28c8f01d24c4e1c8f3a31bebf
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-21 00:41:01 +0530

    Time: 24 ms (22.12%), Space: 8 MB (87.68%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,37 +1,11 @@
 class Solution {
 public:
-    // unordered_map<char,int> freq;
-    //  static bool compare(const char &x,const char &y){
-    //     return freq[x]>freq[y];
-    // }
-    static bool compare(const pair<char,int> &a,const pair<char,int> &b){
-        return a.second>b.second;
-    }
     string frequencySort(string s) {
-        // int n=s.size();
-        // for(int i=0;i<n;i++){
-        //     freq[s[i]]++;
-        // }
-        // sort(s.begin(),s.end(),compare);
-        // return s;
-        int n=s.size();
-        unordered_map<char,int> mp;
-        for(int i=0;i<n;i++){
-            mp[s[i]]++;
-        }
-        vector<pair<char,int>> v;
-        for(auto &it:mp){
-            v.push_back({it.first,it.second});
-        }
-        sort(v.begin(),v.end(),compare);
-        string ans;
-        for(int i=0;i<v.size();i++){
-            int k=0;
-            while(k<v[i].second){
-                ans.push_back(v[i].first);
-                k++;
-            }
-        }
-        return ans;
+        int cnt[128]={};
+        for(auto c:s)cnt[c]++;
+        sort(s.begin(),s.end(),[&](char a,char b){
+         return cnt[a]>cnt[b]||cnt[a]==cnt[b]&&a<b;
+        });
+        return s;
     }
 };
