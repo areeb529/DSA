3b04a1621b03227c5b3ab9a703c4f5b07c699931
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-24 15:39:44 +0530

    Time: 7 ms (67.18%), Space: 8.6 MB (87.14%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,13 +3,13 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
         int n=m;
-        vector<vector<int>> dp(m,vector<int>(n));
+        vector<int> dp(n);
         for(int i=m-1;i>=0;i--){
             for(int j=0;j<=i;j++){
-                if(i==m-1)dp[i][j]=triangle[i][j];
-                else dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+                if(i==m-1)dp[j]=triangle[i][j];
+                else dp[j]=triangle[i][j]+min(dp[j],dp[j+1]);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
