e6903404ae96e873940e5c7e546b0d77aaba0ef0
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-01-24 15:35:03 +0530

    Time: 11 ms (26.26%), Space: 9.1 MB (9.10%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle,int m,int n,int i,int j,vector<vector<int>> &dp){
-        if(i==m-1)return triangle[i][j];
-        if(dp[i][j]!=-1)return dp[i][j];
-        int op1=minimumTotal(triangle,m,n,i+1,j,dp);
-        int op2=minimumTotal(triangle,m,n,i+1,j+1,dp);
-        return dp[i][j]=triangle[i][j]+min(op1,op2);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
-        int n=triangle[m-1].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return minimumTotal(triangle,m,n,0,0,dp);
+        int n=m;
+        vector<vector<int>> dp(m,vector<int>(n));
+        for(int i=m-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                if(i==m-1)dp[i][j]=triangle[i][j];
+                else dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
