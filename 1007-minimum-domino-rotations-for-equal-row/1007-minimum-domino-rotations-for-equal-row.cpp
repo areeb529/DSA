4549e58f440becbcf7516e8591a5e53c31bc21ba
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-03-20 19:51:50 +0530

    Time: 112 ms (92.82%), Space: 111.5 MB (94.45%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    int helper(vector<int>& tops, vector<int>& bottoms,int diceValue,int n){
-        int topEqualValueRotations=0;
-        for(int i=0;i<n;i++){
-            if(tops[i]!=diceValue)topEqualValueRotations++;
-        }
-        int bottomEqualValueRotations=0;
-        for(int i=0;i<n;i++){
-            if(bottoms[i]!=diceValue)bottomEqualValueRotations++;
-        }
-        return min(topEqualValueRotations,bottomEqualValueRotations);
-    }
+    
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         int n=tops.size();
-        //vector<int> freq={0,0,0,0,0,0,0};
-        vector<int> freq(7,0);
-        vector<int> equalRowValue;
+        vector<int> cntA(7,0), cntB(7,0), same(7,0);
         for(int i=0;i<n;i++){
-            if(tops[i]==bottoms[i]){
-                freq[tops[i]]++;
-                if(freq[tops[i]]==n)equalRowValue.push_back(tops[i]);
-            }
-            else{
-                freq[tops[i]]++;
-                if(freq[tops[i]]==n)equalRowValue.push_back(tops[i]);
-                freq[bottoms[i]]++;
-                if(freq[bottoms[i]]==n)equalRowValue.push_back(bottoms[i]);
-            }
+            cntA[tops[i]]++;
+            cntB[bottoms[i]]++;
+            if(tops[i]==bottoms[i])same[tops[i]]++;
         }
-        if(equalRowValue.size()==0)return -1;
         int ans=INT_MAX;
-        for(int i=0;i<equalRowValue.size();i++){
-            ans=min(ans,helper(tops,bottoms,equalRowValue[i],n));
+        for(int i=1;i<=6;i++){
+            if(cntA[i]+cntB[i]-same[i]==n){
+                ans=min(ans,n-max(cntA[i],cntB[i]));
+            }
         }
-        return ans;
+        return ans==INT_MAX?-1:ans;
     }
 };
