b1e13977e06dbfceabd5ca8ed08137ee2e47c21c
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-03-22 23:31:28 +0530

    Time: 610 ms (6.57%), Space: 102.1 MB (19.96%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -1,72 +1,53 @@
 class Solution {
 public:
     long long minimalKSum(vector<int>& nums, int k) {
-        // sort(nums.begin(),nums.end());
-        // int n=nums.size();
-        // long long res=0;
-        // for(int i=0;i<n&&k;i++){
-        //     long long diff;
-        //     if(i==0){
-        //         diff=nums[i]-1;
-        //         k-=diff;
-        //         res+=(long)diff*(diff+1)/2;
-        //         cout<<k<<":"<<res<<" ";
-        //     }
-        //     else{
-        //         diff=(nums[i]-nums[i-1]-1);
-        //         if(diff<=k){
-        //         k-=diff;
-        //         res+=((long)nums[i]*(nums[i]+1)/2-(long)nums[i-1]*(nums[i-1]+1)/2-nums[i]);
-        //             cout<<k<<":"<<res<<" ";
-        //         }
-        //         else{
-        //             res+=((long)(nums[i-1]+k)*(nums[i-1]+k+1)/2-(long)nums[i-1]*(nums[i-1]+1)/2-nums[i-1]-k);
-        //             cout<<k<<":"<<res<<" ";
-        //             k=0;
-        //         }
-        //     }
-        // }
-        // if(k){
-        //     res+=((long)(nums[n-1]+k)*(nums[n-1]+k+1)/2-(long)nums[n-1]*(nums[n-1]+1)/2);
-        //     cout<<k<<":"<<res<<" ";
-        // }
-        // cout<<endl;
-        // return res;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        long long res=0;
-        for(int i=0;i<n&&k;i++){
-            long long diff;
-            if(i==0){
-                diff=nums[0]-1;
-                if(diff>=k){
-                    res=res+(long)k*(k+1)/2;
-                    k=0;
-                }
-                else{
-                    res=res+diff*(diff+1)/2;
-                    k-=diff;
-                }
+//         sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//         long long res=0;
+//         for(int i=0;i<n&&k;i++){
+//             long long diff;
+//             if(i==0){
+//                 diff=nums[0]-1;
+//                 if(diff>=k){
+//                     res=res+(long)k*(k+1)/2;
+//                     k=0;
+//                 }
+//                 else{
+//                     res=res+diff*(diff+1)/2;
+//                     k-=diff;
+//                 }
                 
-            }
-            else{
-                diff=nums[i]-nums[i-1]-1;
-                if(diff<=k&&diff!=-1){
-                    k-=diff;
-                    res=res+(long)nums[i]*(nums[i]+1)/2-(long)nums[i-1]*(nums[i-1]+1)/2-nums[i];
-                }
-                else{
-                    if(diff!=-1){
-                        res=res+(long)(nums[i-1]+k)*(nums[i-1]+k+1)/2-(long)nums[i-1]*(nums[i-1]+1)/2;
-                        k=0;
-                    }
-                }
-            }
+//             }
+//             else{
+//                 diff=nums[i]-nums[i-1]-1;
+//                 if(diff<=k&&diff!=-1){
+//                     k-=diff;
+//                     res=res+(long)nums[i]*(nums[i]+1)/2-(long)nums[i-1]*(nums[i-1]+1)/2-nums[i];
+//                 }
+//                 else{
+//                     if(diff!=-1){
+//                         res=res+(long)(nums[i-1]+k)*(nums[i-1]+k+1)/2-(long)nums[i-1]*(nums[i-1]+1)/2;
+//                         k=0;
+//                     }
+//                 }
+//             }
             
+//         }
+//         if(k){
+//             res=res+(long)(nums[n-1]+k)*(nums[n-1]+k+1)/2-(long)(nums[n-1])*(nums[n-1]+1)/2;
+//         }
+//         return res;
+        set<int> s(nums.begin(),nums.end());
+        long long ans=(long)k*(k+1)/2;
+        for(auto &i:s){
+            if(i<=k){
+                ans+=k+1;
+                ans-=i;
+                k++;
+            }
+            else break;
         }
-        if(k){
-            res=res+(long)(nums[n-1]+k)*(nums[n-1]+k+1)/2-(long)(nums[n-1])*(nums[n-1]+1)/2;
-        }
-        return res;
+        return ans;
+        
     }
 };
