414cf2a114d724fb1f68d1a0b450eea29486b01d
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-08 23:03:46 +0530

    Time: 40 ms (8.52%), Space: 5.7 MB (91.53%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int num=0;
-        int org=x;
-        while(org){
-            if(num>INT_MAX/10)return false;
-            if(x>=0&&num==INT_MAX/10){
-                if(org%10>(INT_MAX%10))return false;
-            }
-            if(x<0&&num==INT_MAX/10){
-                if(org%10>(INT_MAX%10+1))return false;
-            }
-            if(org==INT_MIN){
-                num=INT_MAX%10+1;
-            }
-            else{
-               num=num*10+org%10; 
-            }
-            org/=10;
+        if(x==0)return true;
+        if(x<0||x%10==0)return false;
+        int sum1=abs(x);
+        int sum2=0;
+        while(sum1>sum2){
+            sum2=sum2*10+sum1%10;
+            sum1/=10;
         }
-        return num==abs(x);
+        return sum1==sum2||sum2/10==sum1;
     }
 };
