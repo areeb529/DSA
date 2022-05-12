9aec791f181402862f8f33956f14be6593077b7a
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-12 18:52:09 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (89.89%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minTimeToType(string word) {
-        int n=word.size();
-        int ans=0;
-        ans=ans+min(word[0]-'a',1+'z'-word[0]);
-        ans++;
-        for(int i=1;i<n;i++){
-            if(word[i]>word[i-1]){
-                ans=ans+min(word[i]-word[i-1],'z'-word[i]+word[i-1]-'a'+1);
-            }
-            else{
-                ans=ans+min(word[i-1]-word[i],'z'-word[i-1]+word[i]-'a'+1);
-            }
-            ans++;
+        int res=word.size();
+        char point='a';
+        for(auto &ch:word){
+            res=res+min(abs(ch-point),26-abs(point-ch));
+            point=ch;
         }
-        return ans;
+        return res;
     }
 };
