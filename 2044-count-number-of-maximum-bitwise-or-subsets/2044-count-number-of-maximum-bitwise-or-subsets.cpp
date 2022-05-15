7f47e9e29874f207d103e40fcb355a3b881c07d8
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-15 20:16:09 +0530

    Time: 124 ms (43.84%), Space: 8.3 MB (83.10%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int res=0;
-    void helper(vector<int>& nums,int i,int n,int maxOR,int bitOR){
-        if(i==n){
-            if(bitOR==maxOR)res++;
-            return;
-        }
-        helper(nums,i+1,n,maxOR,bitOR);
-        helper(nums,i+1,n,maxOR,bitOR|nums[i]);
-    }
+    
     int countMaxOrSubsets(vector<int>& nums) {
         int n=nums.size();
         int maxOR=0;
         for(int i=0;i<n;i++){
             maxOR|=nums[i];
         }
-        int bitOR=0;
-        helper(nums,0,n,maxOR,bitOR);
+        int res=0;
+        for(int i=0;i<(1<<n);i++){
+            int bitOR=0;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j))bitOR=bitOR|nums[j];
+            }
+            if(bitOR==maxOR)res++;
+        }
         return res;
+        
     }
 };
