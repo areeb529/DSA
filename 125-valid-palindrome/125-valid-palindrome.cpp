496af75112c0e08afdfbcd76e3d0b81a27007495
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-18 13:42:39 +0530

    Time: 3 ms (95.01%), Space: 7.4 MB (49.42%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    char toLower(char c){
-       if(c>='a'&&c<='z'||c>='0'&&c<='9')return c;
-       else if(c>='A'&&c<='Z'){
-           c='a'+c-'A';
-           return c;
-       }
-        else{
-            return '*';
-        }
-    }
+    
     bool isPalindrome(string s) {
         int n=s.size();
         int i=0,j=n-1;
         while(i<j){
-            char ch1=toLower(s[i]);
-            char ch2=toLower(s[j]);
-            if(ch1=='*')i++;
-            else if(ch2=='*')j--;
-            else{
-                if(ch1!=ch2)return false;
-                i++;
-                j--;
-            }
+            while(!isalnum(s[i])&&i<j)i++;
+            while(!isalnum(s[j])&&i<j)j--;
+            if(tolower(s[i])!=tolower(s[j]))return false;
+            i++;
+            j--;
         }
         return true;
     }
