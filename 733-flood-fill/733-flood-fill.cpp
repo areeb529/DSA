b2f020f07d87112b0cf02a5d3abc7d5462b28962
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-18 21:09:05 +0530

    Time: 26 ms (5.49%), Space: 14 MB (50.99%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image,int x,int y,int origColor,int m,int n,vector<vector<bool>> &visited){
-        if(x<0||x>=m||y<0||y>=n||image[x][y]!=origColor||visited[x][y])return;
-        visited[x][y]=true;
-        dfs(image,x+1,y,origColor,m,n,visited);
-        dfs(image,x,y+1,origColor,m,n,visited);
-        dfs(image,x-1,y,origColor,m,n,visited);
-        dfs(image,x,y-1,origColor,m,n,visited);
+    void dfs(vector<vector<int>>& image,int x,int y,int origColor,int newColor,int m,int n){
+        if(x<0||x>=m||y<0||y>=n||image[x][y]!=origColor)return;
+        image[x][y]=newColor;
+        dfs(image,x+1,y,origColor,newColor,m,n);
+        dfs(image,x,y+1,origColor,newColor,m,n);
+        dfs(image,x-1,y,origColor,newColor,m,n);
+        dfs(image,x,y-1,origColor,newColor,m,n);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         if(image[sr][sc]==newColor)return image;
         int m=image.size();
         int n=image[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
-        dfs(image,sr,sc,image[sr][sc],m,n,visited);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visited[i][j]){
-                    image[i][j]=newColor;
-                }
-            }
-        }
+        dfs(image,sr,sc,image[sr][sc],newColor,m,n);
         return image;
     }
 };
