9459eaa4f98779b3dc7adef2be8e0d93d4fd46c6
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-23 10:20:45 +0530

    Time: 13 ms (30.60%), Space: 15.3 MB (12.31%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices,int i,int buy,int n,vector<vector<int>> &dp){
-        if(i==n)return 0;
-        if(dp[buy][i]!=-1)return dp[buy][i];
-        int profit=0;
-        if(buy){
-            int op1=-prices[i]+maxProfit(prices,i+1,0,n,dp);
-            int op2=maxProfit(prices,i+1,1,n,dp);
-            return dp[buy][i]=max(op1,op2);
-        }
-        else{
-            int op1=prices[i]+maxProfit(prices,i+1,1,n,dp);
-            int op2=maxProfit(prices,i+1,0,n,dp);
-            return dp[buy][i]=max(op1,op2);
-        }
-    }
+   // int maxProfit(vector<int>& prices,int i,int buy,int n,vector<vector<int>> &dp){
+   //      if(i==n)return 0;
+   //      if(dp[buy][i]!=-1)return dp[buy][i];
+   //      int profit=0;
+   //      if(buy){
+   //          int op1=-prices[i]+maxProfit(prices,i+1,0,n,dp);
+   //          int op2=maxProfit(prices,i+1,1,n,dp);
+   //          return dp[buy][i]=max(op1,op2);
+   //      }
+   //      else{
+   //          int op1=prices[i]+maxProfit(prices,i+1,1,n,dp);
+   //          int op2=maxProfit(prices,i+1,0,n,dp);
+   //          return dp[buy][i]=max(op1,op2);
+   //      }
+   //  }
     int maxProfit(vector<int>& prices) {
-        // int n=prices.size();
-        // int ans=0;
-        // int boughtStockPrice=prices[0];
-        // for(int i=0;i<n;i++){
-        //     if(prices[i]>boughtStockPrice){
-        //         ans=ans+prices[i]-boughtStockPrice;
-        //     }
-        //     boughtStockPrice=prices[i];
-        // }
-        // return ans;
         int n=prices.size();
-        vector<vector<int>> dp(2,vector<int>(n,-1));
-        return maxProfit(prices,0,1,n,dp);
-        
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                int op1=-prices[i]+dp[i+1][0];
+                int op2=dp[i+1][1];
+                dp[i][buy]=max(op1,op2);
+                }
+                else{
+                int op1=prices[i]+dp[i+1][1];
+                int op2=dp[i+1][0];
+                dp[i][buy]=max(op1,op2);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
