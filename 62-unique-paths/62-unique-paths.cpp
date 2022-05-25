e42a7ba5e8c20cbb075f655a3274bada37e19c7d
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-25 11:25:02 +0530

    Time: 6 ms (10.25%), Space: 5.8 MB (90.99%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+    int combinatorial(long n,int r){
+        if(r==0)return 1;
+        return (n*combinatorial(n-1,r-1))/r;
+    }
     int uniquePaths(int m, int n) {
-        vector<int> dp(n+1,0);
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(i==m-1&&j==n-1)dp[j]=1;
-                else dp[j]=dp[j]+dp[j+1];
-            }
-        }
-        return dp[0];
+//         vector<int> dp(n+1,0);
+//         int X=0,Y=0;
+//         for(int i=m-1;i>=0;i--){
+//             for(int j=n-1;j>=0;j--){
+//                 if(i==m-1&&j==n-1)X=1;
+//                 else X=X+Y;
+                
+//             }
+//         }
+//         return Y;
+        return combinatorial(m+n-2,m-1);
     }
 };
