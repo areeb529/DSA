5ff7d96b6afb080854429b510b18910af8eab16a
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-05-25 10:17:04 +0530

    Time: 8 ms (5.84%), Space: 6.6 MB (26.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int dfs(int i,int j,int m,int n,vector<vector<int>> &dp){
-        if(i==m-1&&j==n-1)return 1;
-        if(i>=m||j>=n)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        return dp[i][j]=dfs(i+1,j,m,n,dp)+dfs(i,j+1,m,n,dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        int ans=dfs(0,0,m,n,dp);
-        return ans;
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1&&j==n-1)dp[i][j]=1;
+                else dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
