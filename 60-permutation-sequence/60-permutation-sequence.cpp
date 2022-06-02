cffd620ff5ad7d8c50ef4d2fab419b3ff77a9958
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-02 20:21:06 +0530

    Time: 380 ms (7.97%), Space: 6 MB (80.24%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
+    int factorial(int n,vector<int> &fact){
+        if(n<=1)return 1;
+        if(fact[n]!=0)return fact[n];
+        return fact[n]=n*factorial(n-1,fact);
+    }
     string getPermutation(int n, int k) {
         string s;
         for(int i=1;i<=n;i++){
             s.push_back(i+'0');
         }
-        k--;
-        while(k--){
-            int i=n-1;
-            while(i>0&&s[i-1]>s[i])i--;
-            if(i>0){
-                int j=i;
-                while(j<n&&s[i-1]<s[j])j++;
-                swap(s[i-1],s[j-1]);
-                sort(s.begin()+i,s.end());
+        vector<int> fact(n+1,0);
+        int factor=factorial(n,fact);
+        fact[0]=1;
+        fact[1]=1;
+        if(k==1)return s;
+        int idx=0;
+        string ans;
+        for(int i=1;i<=n;i++){
+            if(fact[i]==k){
+                reverse(s.begin()+n-i,s.end());
+                return s;
+            }
+            else if(fact[i]<k&&(i==n||fact[i+1]>k)){
+                int cnt=k-fact[i];
+                reverse(s.begin()+n-i,s.end());
+                while(cnt--){
+                    int j=n-1;
+                    while(j>0&&s[j-1]>s[j])j--;
+                    int q=j-1;
+                    if(j>0){
+                        while(j<n&&s[q]<s[j])j++;
+                        swap(s[q],s[j-1]);
+                        sort(s.begin()+q+1,s.end());
+                    }
+                }
+                break;
             }
         }
         return s;
-        
     }
 };
