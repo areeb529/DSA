2a19b9dfe3514ef5bb5c8cf3c03000caef69a6b8
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-03 23:02:09 +0530

    Time: 53 ms (27.45%), Space: 6.5 MB (54.44%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    vector<vector<char>> mat;
-    bool canPlace(vector<vector<char>>& board,char val,int row,int col,int n){
-        for(int i=0;i<n;i++){
+    bool canPlace(vector<vector<char>>& board,char val,int row,int col){
+        for(int i=0;i<9;i++){
             if(board[i][col]==val||board[row][i]==val)return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val)return false;
         }
-        int subMatStartX=3*(row/3),subMatStartY=3*(col/3);
-        for(int i=subMatStartX;i<subMatStartX+3;i++){
-            for(int j=subMatStartY;j<subMatStartY+3;j++){
-                if(board[i][j]==val)return false;
+        return true;
+    }
+    bool helper(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(canPlace(board,c,i,j)){
+                            board[i][j]=c;
+                            if(helper(board)==true)return true;
+                            else board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
             }
         }
         return true;
     }
-    void helper(vector<vector<char>>& board,int row,int col,int n){
-        if(row==n){
-            mat=board;
-            return;
-        }
-        if(board[row][col]!='.')helper(board,row+(col+1)/n,(col+1)%n,n);
-        else{
-            for(char i='1';i<='9';i++){
-                if(canPlace(board,i,row,col,n)){
-                    board[row][col]=i;
-                    helper(board,row+(col+1)/n,(col+1)%n,n);
-                    board[row][col]='.';
-                }
-            }
-        }
-    }
     void solveSudoku(vector<vector<char>>& board) {
         int n=board.size();
-        helper(board,0,0,n);
-        board=mat;
+        bool isSolved=helper(board);
     }
 };
