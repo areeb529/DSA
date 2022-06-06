e6290078021c7c2a8401213aaa0f146447de46c8
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-06 22:57:42 +0530

    Time: 14 ms (5.04%), Space: 11.2 MB (30.07%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -5,19 +5,18 @@ public:
         int ans=-1;
         int low=0,high=n-1;
         while(low<=high){
-            int mid=((high-low)>>1)+low;
-            if(nums[low]==target)return low;
-            else if(nums[high]==target)return high;
-            else if(nums[mid]==target)return mid;
-            else if(nums[low]<nums[mid]){
-                if(nums[low]<=target&&target<nums[mid])high=mid-1;
-                else low=mid+1;
-            }
+            int mid=(high-low)/2+low;
+            int comparator=nums[mid];
+            if((nums[0]<=nums[mid])==(nums[0]<=target))comparator=nums[mid];
             else{
-                if(nums[mid]<target&&target<=nums[high])low=mid+1;
-                else high=mid-1;
+                if(nums[0]<=target)comparator=INT_MAX;
+                else comparator=INT_MIN;
             }
+            if(target<comparator)high=mid-1;
+            else if(target>comparator)low=mid+1;
+            else return mid;
         }
         return ans;
+        
     }
 };
