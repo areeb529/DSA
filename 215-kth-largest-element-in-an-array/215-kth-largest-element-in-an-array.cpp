a83a737bd5692c8e6f6265ca79490dbbaa53e31b
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-07 00:14:25 +0530

    Time: 130 ms (8.91%), Space: 10 MB (61.52%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -2,17 +2,36 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int n=nums.size();
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int i=0;i<k;i++){
-            pq.push(nums[i]);
-        }
-        for(int i=k;i<n;i++){
-            if(nums[i]>pq.top()){
-                pq.pop();
-                pq.push(nums[i]);
+        int low=0,high=n-1;
+        while(low<=high){
+            int ct=0;
+            for(int i=low+1;i<=high;i++){
+                if(nums[i]>=nums[low])ct++;
             }
+            int pivotIndex=low+ct;
+            int pivot=nums[low];
+            if(pivotIndex==k-1)return pivot;
+            else{
+                swap(nums[low],nums[pivotIndex]);
+                int i=low,j=high;
+                while(i<pivotIndex&&j>pivotIndex){
+                    if(nums[i]>=pivot)i++;
+                    else if(pivot>nums[j])j--;
+                    else{
+                        swap(nums[i],nums[j]);
+                        i++;
+                        j--;
+                    }
+                }
+                if(pivotIndex<k-1){
+                    low=pivotIndex+1;
+                }
+                else{
+                    high=pivotIndex-1;
+                }
+            }
+            
         }
-        int ans=pq.top();
-        return ans;
+        return nums[0];
     }
 };
