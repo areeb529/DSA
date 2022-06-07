a896583a45507e3c33589b18bf1c769f3e0415f5
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-07 16:48:22 +0530

    Time: 22 ms (51.96%), Space: 15.5 MB (6.85%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    #define pii   pair<int,int>
-    class compare{
-      public:
-        bool operator()(const pii &a,const pii &b){
-            return a.second>b.second||a.second==b.second&&a.first<b.second;
-        }
-    };
+    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n=nums.size();
         unordered_map<int,int> mp;
         for(int i=0;i<n;i++)mp[nums[i]]++;
-        priority_queue<pii,vector<pii>,compare> pq;
+        vector<vector<int>> bucket(n+1);
         for(auto &i:mp){
-            if(pq.size()<k){
-                pq.push({i.first,i.second});
-            }
-            else{
-                if(pq.top().second<i.second){
-                    pq.pop();
-                    pq.push({i.first,i.second});
-                }
-            }
+            bucket[i.second].push_back(i.first);
         }
         vector<int> res;
-        while(!pq.empty()){
-            res.push_back(pq.top().first);
-            pq.pop();
+        for(int i=n;i>=0&&k;i--){
+            for(auto &j:bucket[i]){
+                res.push_back(j);
+                k--;
+                if(k==0)break;
+            }
         }
         return res;
     }
