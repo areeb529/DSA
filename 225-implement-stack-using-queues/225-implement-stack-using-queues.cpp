4b75b638299fbf05b11b25ca53ba74133b0bbf5e
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-10 01:24:49 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (17.24%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,31 +1,33 @@
 class MyStack {
 public:
-    queue<int> q;
+    queue<int> q1;
+    queue<int> q2;
     MyStack() {
         
     }
-    
     void push(int x) {
-        q.push(x);
-        for(int i=0;i<q.size()-1;i++){
-            q.push(q.front());
-            q.pop();
+        swap(q1,q2);
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
         }
+        
     }
     
     int pop() {
-        int ans=q.front();
-        q.pop();
+        int ans=q1.front();
+        q1.pop();
         return ans;
     }
     
     int top() {
-        int ans=q.front();
+        int ans=q1.front();
         return ans;
     }
     
     bool empty() {
-        return q.size()==0;
+        return (q1.size()+q2.size())==0;
     }
 };
 
