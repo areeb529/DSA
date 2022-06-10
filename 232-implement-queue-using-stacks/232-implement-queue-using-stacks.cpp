575f0ffd325e72960b5b9a70b967a1c5e22873a4
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-10 10:33:09 +0530

    Time: 3 ms (43.91%), Space: 7 MB (55.85%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -7,30 +7,35 @@ public:
     }
     
     void push(int x) {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        s2.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
+       s1.push(x);
     }
     
     int pop() {
-        int ans=s1.top();
-        s1.pop();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int ans=s2.top();
+        s2.pop();
         return ans;
+        
     }
     
     int peek() {
-        int ans=s1.top();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int ans=s2.top();
         return ans;
     }
     
     bool empty() {
-        return s1.size()==0;
+        return s1.size()+s2.size()==0;
     }
 };
 
