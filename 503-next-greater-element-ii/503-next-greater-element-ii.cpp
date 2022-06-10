7842701998cafec4e697fbfb47bdcabdaf7912b5
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-10 16:47:44 +0530

    Time: 57 ms (34.81%), Space: 23.8 MB (89.22%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -3,25 +3,14 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         stack<int> st;
-        vector<int> res(n,INT_MIN);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&&nums[i]>=st.top()){
+        vector<int> res(n,-1);
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty()&&nums[i%n]>=st.top()){
                 st.pop();
             }
-            if(st.empty()||nums[i]<st.top()){
-                if(!st.empty())res[i]=st.top();
-                st.push(nums[i]);
-            }
-        }
-        int j=0;
-        for(int i=n-1;i>=0;i--){
-            if(res[i]==INT_MIN){
-                int x=nums[i];
-                while(j<n&&nums[j]<=x){
-                    j++;
-                }
-                if(j<n)res[i]=nums[j];
-                else res[i]=-1;
+            if(st.empty()||nums[i%n]<st.top()){
+                if(!st.empty())res[i%n]=st.top();
+                st.push(nums[i%n]);
             }
         }
         return res;
