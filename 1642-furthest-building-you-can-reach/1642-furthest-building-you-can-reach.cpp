63995e80c4be3aee32ff897874c71ddde285635d
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-22 01:54:45 +0530

    Time: 142 ms (59.83%), Space: 53.6 MB (94.68%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
-    // bool canReach(vector<int>& heights, int n,int bricks, int ladders,int targetIndex){
-    //     vector<int> heightDiff;
-    //     long long totalPositiveDiff=0;
-    //     for(int i=1;i<=targetIndex;i++){
-    //         long long diff=heights[i]-heights[i-1];
-    //         if(diff>0){
-    //             heightDiff.push_back(diff);
-    //             totalPositiveDiff+=diff;
-    //         }
-    //     }
-    //     long long heightCoveredByLadder=0;
-    //     sort(heightDiff.begin(),heightDiff.end(),greater<int>());
-    //     for(int i=0;i<ladders&&i<heightDiff.size();i++){
-    //         heightCoveredByLadder+=heightDiff[i];
-    //     }
-    //     if(totalPositiveDiff-heightCoveredByLadder<=bricks)return true;
-    //     else return false;
-    // }
+    
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int n=heights.size();
-        priority_queue<int,vector<int>,greater<int>> pq;
-        long long heightCoveredByLadder=0;
-        long long totalPositiveDiff=0;
-        int ans=n-1;
-        for(int i=1;i<n;i++){
-            long long diff=heights[i]-heights[i-1];
-            if(diff>0){
-                totalPositiveDiff+=diff;
-                if(pq.size()<ladders){
-                    pq.push(diff);
-                    heightCoveredByLadder+=diff;
-                }
-                else{
-                    if(!pq.empty()&&pq.top()<diff){
-                        heightCoveredByLadder-=pq.top();
-                        pq.pop();
-                        pq.push(diff);
-                        heightCoveredByLadder+=diff;
-                    }
-                }
-                if(totalPositiveDiff-heightCoveredByLadder>bricks){
-                    ans=i-1;
-                    break;
-                }
+        priority_queue<int> pq;
+        for(int i=0;i<n-1;i++){
+            int d=heights[i+1]-heights[i];
+            if(d>0){
+                pq.push(-d);
             }
+            if(pq.size()>ladders){
+                bricks+=pq.top();
+                pq.pop();
+            }
+            if(bricks<0)return i;
         }
-        return ans;
+        return n-1;
+        
     }
 };
