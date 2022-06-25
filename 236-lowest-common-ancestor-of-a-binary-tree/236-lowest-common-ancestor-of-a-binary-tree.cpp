a597473626f76ef25ddaf3451510c07e0a593015
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-25 16:37:10 +0530

    Time: 19 ms (75.78%), Space: 17.5 MB (5.24%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,25 +9,25 @@
  */
 class Solution {
 public:
+    bool getPath(TreeNode*root,TreeNode*node,vector<TreeNode*> &v){
+        if(!root)return false;
+        v.push_back(root);
+        if(root==node)return true;
+        bool op1=getPath(root->left,node,v);
+        bool op2=getPath(root->right,node,v);
+        if(op1||op2)return true;
+        v.pop_back();
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // if(!root)return NULL;
-        // else if(root==p||root==q)return root;
-        // else {
-        //     TreeNode*lcaOnLeft=lowestCommonAncestor(root->left,p,q);
-        //     if(lcaOnLeft)return lcaOnLeft;
-        //     TreeNode*lcaOnRight=lowestCommonAncestor(root->right,p,q);
-        //     if(lcaOnRight)return lcaOnRight;
-        //     return root;
-        // }
-        if(!root)return NULL;
-        if(root==p||root==q)return root;
-        TreeNode*lcaOnLeft=lowestCommonAncestor(root->left,p,q);
-        TreeNode*lcaOnRight=lowestCommonAncestor(root->right,p,q);
-        if(lcaOnLeft&&lcaOnRight)return root;
-        else if(lcaOnLeft)return lcaOnLeft;
-        else if(lcaOnRight)return lcaOnRight;
-        else return NULL;
-        
-        
+        vector<TreeNode*> pathToP;
+        bool f1=getPath(root,p,pathToP);
+        vector<TreeNode*> pathToQ;
+        bool f2=getPath(root,q,pathToQ);
+        TreeNode*lca=NULL;
+        for(int i=0;i<min(pathToP.size(),pathToQ.size())&&pathToP[i]==pathToQ[i];i++){
+            lca=pathToP[i];
+        }
+        return lca;
     }
 };
