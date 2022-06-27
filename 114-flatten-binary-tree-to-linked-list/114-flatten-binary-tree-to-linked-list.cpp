b177d53d4263d87da3e8b55beff7ccd6d21e91f0
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-27 14:35:33 +0530

    Time: 3 ms (97.03%), Space: 12.8 MB (39.23%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,44 +11,34 @@
  */
 class Solution {
 public:
-    TreeNode*tail=NULL;
-    TreeNode*flattenTree(TreeNode*root){
-        // if(!root)return root;
-        // if(root->right==NULL)tail=root;
-        // TreeNode*leftS=flattenTree(root->left);
-        // root->left=NULL;
-        // TreeNode*rightChild=root->right;
-        // root->right=leftS;
-        // TreeNode*temp=tail;
-        // TreeNode*rightS=flattenTree(rightChild);
-        // if(temp){
-        //     temp->right=rightS;
-        // }
-        // else{
-        //     root->right=rightS;
-        // }
-        // return root;
-        if(!root)return root;
-        if(root->right==NULL){
-            tail=root;
-        }
-        TreeNode*leftS=flattenTree(root->left);
-        if(leftS==NULL)tail=root;
-        root->left=NULL;
-        TreeNode*temp=tail;
-        TreeNode*rightS=flattenTree(root->right);
-        if(temp){
-            root->right=leftS;
-            temp->right=rightS;
-        }
-        else{
-            root->right=rightS;
-        }
-        return root;
+//     TreeNode*tail=NULL;
+//     TreeNode*flattenTree(TreeNode*root){
+//         if(!root)return root;
+//         if(root->right==NULL){
+//             tail=root;
+//         }
+//         TreeNode*leftS=flattenTree(root->left);
+//         if(leftS==NULL)tail=root;
+//         root->left=NULL;
+//         TreeNode*temp=tail;
+//         TreeNode*rightS=flattenTree(root->right);
+//         if(temp){
+//             root->right=leftS;
+//             temp->right=rightS;
+//         }
+//         else{
+//             root->right=rightS;
+//         }
+//         return root;
         
-    }
+//     }
+    TreeNode*prev=NULL;
     void flatten(TreeNode* root) {
-        root=flattenTree(root);
-        
+        if(!root)return;
+        flatten(root->right);
+        flatten(root->left);
+        root->left=NULL;
+        root->right=prev;
+        prev=root;
     }
 };
