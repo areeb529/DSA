5d69f50bbc074194d10ff69ee182788815149045
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-06-29 11:03:45 +0530

    Time: 19 ms (91.31%), Space: 16.8 MB (86.85%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,17 +19,28 @@ public:
 class Solution {
 public:
     
-    Node*connectNext(Node*root){
-        if(!root||root->left==NULL||root->right==NULL)return root;
-        root->left->next=root->right;
-        if(root->next)
-        root->right->next=root->next->left;
-        Node*leftS=connectNext(root->left);
-        Node*rightS=connectNext(root->right);
-        return root;
-    }
+    // Node*connectNext(Node*root){
+    //     if(!root||root->left==NULL||root->right==NULL)return root;
+    //     root->left->next=root->right;
+    //     if(root->next)
+    //     root->right->next=root->next->left;
+    //     Node*leftS=connectNext(root->left);
+    //     Node*rightS=connectNext(root->right);
+    //     return root;
+    // }
     Node* connect(Node* root) {
         if(!root)return root;
-        return connectNext(root);
+        //return connectNext(root);
+        Node*levelStart=root;
+        while(levelStart){
+            Node*cur=levelStart;
+            while(cur){
+                if(cur->left)cur->left->next=cur->right;
+                if(cur->next&&cur->right)cur->right->next=cur->next->left;
+                cur=cur->next;
+            }
+            levelStart=levelStart->left;
+        }
+        return root;
     }
 };
