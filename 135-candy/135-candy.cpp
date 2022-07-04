044812005f48daeedd4dca7b3f2e1448c9d2a861
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-07-04 11:08:01 +0530

    Time: 23 ms (88.00%), Space: 18.9 MB (10.65%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        //int n=ratings.size();
+        // vector<int> candies(n,1);
+        // for(int i=0;i<n;i++){
+        //     int leftCandies=0,rightCandies=0;
+        //     if(i-1>=0&&ratings[i-1]<ratings[i]){
+        //         candies[i]=candies[i-1]+1;
+        //     }
+        //     if(i+1<n&&ratings[i]>ratings[i+1]){
+        //         if(candies[i]<=candies[i+1]){
+        //             candies[i]=candies[i+1]+1;
+        //         }
+        //     }
+        // }
+        // int res=0;
+        // for(int i=0;i<n;i++){
+        //     cout<<candies[i]<<" ";
+        //     res+=candies[i];
+        // }
+        // cout<<endl;
+        // return res;
+        int n=ratings.size();
+        vector<int> inc(n,1);
+        vector<int> dec(n,1);
+        vector<int> res(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])inc[i]=inc[i-1]+1;
+            if(ratings[n-i-1]>ratings[n-i])dec[n-i-1]=dec[n-i]+1;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            res[i]=max(inc[i],dec[i]);
+            ans+=res[i];
+        }
+        return ans; 
+        
+    }
+};
