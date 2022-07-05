3bb74b3682d104829228c7cd79ac0d1322eec167
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-07-05 11:50:55 +0530

    Time: 2 ms (80.38%), Space: 6.9 MB (42.52%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,50 +1,66 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
+       
+        // stack<char> st;
+        // int n=s.size();
         // unordered_map<char,int> mp;
-        // for(int i=n-1;i>=0;i--){
-        //     mp[s[i]]=i;
+        // unordered_map<char,int> freq;
+        // for(int i=0;i<n;i++){
+        //     freq[s[i]]++;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(st.empty()){
+        //         st.push(s[i]);
+        //         freq[s[i]]--;
+        //         if(freq[s[i]]==0)freq.erase(s[i]);
+        //         mp[s[i]]=i;
+        //     }
+        //     else{
+        //         if(mp.count(s[i])==1){
+        //             freq[s[i]]--;
+        //             if(freq[s[i]]==0)freq.erase(s[i]);
+        //         }
+        //         if(s[i]>st.top()&&mp.count(s[i])==0){
+        //             st.push(s[i]);
+        //             freq[s[i]]--;
+        //             if(freq[s[i]]==0)freq.erase(s[i]);
+        //             mp[s[i]]=i;
+        //         }
+        //         while(!st.empty()&&s[i]<st.top()&&freq.count(st.top())==1&&mp.count(s[i])==0){
+        //             mp.erase(st.top());
+        //             st.pop();
+        //         }
+        //         if(mp.count(s[i])==0){
+        //             st.push(s[i]);
+        //             freq[s[i]]--;
+        //             if(freq[s[i]]==0)freq.erase(s[i]);
+        //             mp[s[i]]=i;   
+        //         }
+        //     }
         // }
         // string res;
-        // for(int i=n-1;i>=0;i--){
-        //     if(mp[s[i]]==i)
+        // while(!st.empty()){
+        //     res.push_back(st.top());
+        //     st.pop();
         // }
+        // reverse(res.begin(),res.end());
+        // return res;
         stack<char> st;
         int n=s.size();
-        unordered_map<char,int> mp;
-        unordered_map<char,int> freq;
+        vector<bool> seen(26,false);
+        vector<int> lastIndex(26,-1);
         for(int i=0;i<n;i++){
-            freq[s[i]]++;
+            lastIndex[s[i]-'a']=i;
         }
         for(int i=0;i<n;i++){
-            if(st.empty()){
-                st.push(s[i]);
-                freq[s[i]]--;
-                if(freq[s[i]]==0)freq.erase(s[i]);
-                mp[s[i]]=i;
+            if(seen[s[i]-'a'])continue;
+            while(!st.empty()&&s[i]<st.top()&&lastIndex[st.top()-'a']>i){
+                seen[st.top()-'a']=false;
+                st.pop();
             }
-            else{
-                if(mp.count(s[i])==1){
-                    freq[s[i]]--;
-                    if(freq[s[i]]==0)freq.erase(s[i]);
-                }
-                if(s[i]>st.top()&&mp.count(s[i])==0){
-                    st.push(s[i]);
-                    freq[s[i]]--;
-                    if(freq[s[i]]==0)freq.erase(s[i]);
-                    mp[s[i]]=i;
-                }
-                while(!st.empty()&&s[i]<st.top()&&freq.count(st.top())==1&&mp.count(s[i])==0){
-                    mp.erase(st.top());
-                    st.pop();
-                }
-                if(mp.count(s[i])==0){
-                    st.push(s[i]);
-                    freq[s[i]]--;
-                    if(freq[s[i]]==0)freq.erase(s[i]);
-                    mp[s[i]]=i;   
-                }
-            }
+            st.push(s[i]);
+            seen[s[i]-'a']=true;
         }
         string res;
         while(!st.empty()){
