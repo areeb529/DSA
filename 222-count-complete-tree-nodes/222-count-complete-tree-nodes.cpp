09bc383b2ff6179b3055822aee22e36991a9557f
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-07-28 01:15:46 +0530

    Time: 53 ms (46.26%), Space: 30.8 MB (62.85%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,40 +11,56 @@
  */
 class Solution {
 public:
-    int totalNodes;
-    void helper(TreeNode*root,int height){
-        if(root==NULL){
-            totalNodes-=height;
-            return;
-        }
-        TreeNode*node=root->left;
-        // if(node==NULL){
-        //     totalNodes-=2;
-        //     return;
+//     int totalNodes;
+//     void helper(TreeNode*root,int height){
+//         if(root==NULL){
+//             totalNodes-=height;
+//             return;
+//         }
+//         TreeNode*node=root->left;
+   
+//         int subTreeHeight=height-1;
+//         while(node){
+//             node=node->right;
+//             subTreeHeight--;
+//         }
+//         if(subTreeHeight==0){
+//             helper(root->right,height-1);
+//         }
+//         else{
+//             totalNodes-=(1<<(height-2));
+//             helper(root->left,height-1);
+//         }
+//     }
+    int countNodes(TreeNode* root) {
+        // int height=0;
+        // if(root==NULL)return 0;
+        // TreeNode*temp=root;
+        // while(temp){
+        //     height++;
+        //     temp=temp->left;
         // }
-        int subTreeHeight=height-1;
+        // totalNodes=(1<<(height))-1;
+        // helper(root,height);
+        // return totalNodes;
+        if(root==NULL)return 0;
+        TreeNode*node=root->left;
+        int lh=0,rh=0;
+        while(node){
+            node=node->left;
+            lh++;
+        }
+        node=root->right;
         while(node){
             node=node->right;
-            subTreeHeight--;
+            rh++;
         }
-        if(subTreeHeight==0){
-            helper(root->right,height-1);
+        if(lh==rh){
+            return ((1<<(lh+1))-1);
         }
         else{
-            totalNodes-=(1<<(height-2));
-            helper(root->left,height-1);
+            return 1+countNodes(root->left)+countNodes(root->right);
         }
-    }
-    int countNodes(TreeNode* root) {
-        int height=0;
-        if(root==NULL)return 0;
-        TreeNode*temp=root;
-        while(temp){
-            height++;
-            temp=temp->left;
-        }
-        totalNodes=(1<<(height))-1;
-        helper(root,height);
-        return totalNodes;
+        
     }
 };
