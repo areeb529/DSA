cfefd312aab728ddb1aa0a36789015a9fc8e8c91
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-09 14:23:15 +0530

    Time: 46 ms (81.44%), Space: 18.9 MB (84.66%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        sort(intervals.begin(),intervals.end());
-        stack<pair<int,int>> st;
-        
-        for(int i=0;i<n;i++){
-            if(st.empty())st.push({intervals[i][0],intervals[i][1]});
+      int n=intervals.size();
+      vector<vector<int>> res;
+      sort(intervals.begin(),intervals.end());
+       vector<int> newInterval=intervals[0];
+        for(auto &it:intervals){
+            if(it[0]<=newInterval[1]){
+                newInterval[1]=max(newInterval[1],it[1]);
+            }
             else{
-                if(intervals[i][0]<=st.top().second){
-                    st.top().second=max(st.top().second,intervals[i][1]);
-                }
-                else st.push({intervals[i][0],intervals[i][1]});
+                res.push_back(newInterval);
+                newInterval=it;
             }
         }
-        vector<vector<int>> res;
-        while(!st.empty()){
-            res.push_back({st.top().first,st.top().second});
-            st.pop();
-        }
-        reverse(res.begin(),res.end());
+        res.push_back(newInterval);
         return res;
     }
 };
