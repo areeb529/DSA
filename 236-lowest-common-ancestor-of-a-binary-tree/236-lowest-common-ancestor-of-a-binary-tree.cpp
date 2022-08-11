e449a83a96b37ff8869f6976d538a1f3fa8b4872
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-11 11:40:07 +0530

    Time: 32 ms (25.14%), Space: 14.1 MB (97.27%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,25 +9,16 @@
  */
 class Solution {
 public:
-    bool getPath(TreeNode*root,TreeNode*node,vector<TreeNode*> &v){
-        if(!root)return false;
-        v.push_back(root);
-        if(root==node)return true;
-        bool op1=getPath(root->left,node,v);
-        bool op2=getPath(root->right,node,v);
-        if(op1||op2)return true;
-        v.pop_back();
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> pathToP;
-        bool f1=getPath(root,p,pathToP);
-        vector<TreeNode*> pathToQ;
-        bool f2=getPath(root,q,pathToQ);
-        TreeNode*lca=NULL;
-        for(int i=0;i<min(pathToP.size(),pathToQ.size())&&pathToP[i]==pathToQ[i];i++){
-            lca=pathToP[i];
-        }
-        return lca;
+        if(!root)return NULL;
+        if(root==p||root==q)return root;
+        TreeNode*leftS=lowestCommonAncestor(root->left,p,q);
+        TreeNode*rightS=lowestCommonAncestor(root->right,p,q);
+       
+        if(leftS&&rightS)return root;
+        else if(leftS)return leftS;
+        else if(rightS)return rightS;
+        else return NULL;
+        
     }
 };
