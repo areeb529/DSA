e064e283f6d1599cf375b62013bf9c7d912df6fd
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-12 12:27:22 +0530

    Time: 23 ms (95.68%), Space: 24 MB (61.12%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        // int n=nums.size();
+        // vector<int> pre(n,1);
+        // vector<int> suf(n,1);
+        // for(int i=1;i<n;i++){
+        //     pre[i]=pre[i-1]*nums[i-1];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     suf[i]=suf[i+1]*nums[i+1];
+        // }
+        // vector<int> ans(n);
+        // for(int i=0;i<n;i++){
+        //     ans[i]=pre[i]*suf[i];
+        // }
+        // return ans;
+        int prod=1;
         int n=nums.size();
-        vector<int> pre(n,1);
-        vector<int> suf(n,1);
-        for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]*nums[i-1];
-        }
+        vector<int> ans(n,1);
         for(int i=n-2;i>=0;i--){
-            suf[i]=suf[i+1]*nums[i+1];
+            ans[i]=ans[i+1]*nums[i+1];
         }
-        vector<int> ans(n);
+        int pre=1;
         for(int i=0;i<n;i++){
-            ans[i]=pre[i]*suf[i];
+            ans[i]=ans[i]*pre;
+            pre*=nums[i];
         }
         return ans;
     }
