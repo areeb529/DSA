437941ce877e192b89656f2cc8ef96a0b8bf20a6
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-12 18:03:35 +0530

    Time: 7 ms (86.26%), Space: 13.1 MB (46.88%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,59 +1,52 @@
 class Solution {
 public:
+    bool isValid(vector<vector<int>> &grid,int row,int col,int m,int n){
+        if(row<0||row>=m||col<0||col>=n||grid[row][col]!=1)return false;
+        return true;
+    }
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
+        int fresh=0;
         queue<pair<int,int>> pN;
-        int time=0;
-        int numOranges=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    pN.push({i,j});
-                }
-                if(grid[i][j]==1){
-                    numOranges++;
-                }
+                if(grid[i][j]==2)pN.push({i,j});
+                if(grid[i][j]==1)fresh++;
             }
         }
-        if(pN.size()==0&&numOranges)return -1;
-        if(pN.size()==0&&numOranges==0)return 0;
-        while(!pN.empty()){
-           int N=pN.size();
-            bool f=false;
+        int time=0;
+        while(!pN.empty()&&fresh){
+            int N=pN.size();
             for(int i=0;i<N;i++){
                 pair<int,int> front=pN.front();
                 pN.pop();
-                int x=front.first;
-                int y=front.second;
-                if(x+1<m&&grid[x+1][y]==1){
-                    grid[x+1][y]=2;
-                    pN.push({x+1,y});
-                    f=true;
-                    numOranges--;
+                int row=front.first;
+                int col=front.second;
+                if(isValid(grid,row+1,col,m,n)){
+                    grid[row+1][col]=2;
+                    pN.push({row+1,col});
+                    fresh--;
                 }
-                if(y+1<n&&grid[x][y+1]==1){
-                    grid[x][y+1]=2;
-                    pN.push({x,y+1});
-                    f=true;
-                    numOranges--;
+                if(isValid(grid,row,col+1,m,n)){
+                    grid[row][col+1]=2;
+                    pN.push({row,col+1});
+                    fresh--;
                 }
-                if(x-1>=0&&grid[x-1][y]==1){
-                    grid[x-1][y]=2;
-                    pN.push({x-1,y});
-                    f=true;
-                    numOranges--;
+                if(isValid(grid,row-1,col,m,n)){
+                    grid[row-1][col]=2;
+                    pN.push({row-1,col});
+                    fresh--;
                 }
-                if(y-1>=0&&grid[x][y-1]==1){
-                    grid[x][y-1]=2;
-                    pN.push({x,y-1});
-                    f=true;
-                    numOranges--;
+                if(isValid(grid,row,col-1,m,n)){
+                    grid[row][col-1]=2;
+                    pN.push({row,col-1});
+                    fresh--;
                 }
             }
-            if(f)time++;
+            time++;
         }
-        if(numOranges==0)return time;
-        else return -1;
+        if(fresh)return -1;
+        return time;
     }
 };
