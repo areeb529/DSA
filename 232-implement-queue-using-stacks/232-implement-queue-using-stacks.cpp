476d10f3147059be2b84d9e9636642101b4377f4
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-12 13:13:13 +0530

    Time: 8 ms (7.95%), Space: 7 MB (89.78%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -7,29 +7,52 @@ public:
     }
     
     void push(int x) {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
+        // while(!s1.empty()){
+        //     s2.push(s1.top());
+        //     s1.pop();
+        // }
+        // s1.push(x);
+        // while(!s2.empty()){
+        //     s1.push(s2.top());
+        //     s2.pop();
+        // }
         s1.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
     }
     
     int pop() {
-        int ans=s1.top();
-        s1.pop();
+        int ans;
+        if(!s2.empty()){
+            ans=s2.top();
+            s2.pop();
+        }
+        else{
+            while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+            }
+            ans=s2.top();
+            s2.pop();
+        }
         return ans;
     }
     
     int peek() {
-        return s1.top();
+        int ans;
+        if(!s2.empty()){
+            ans=s2.top();
+        }
+        else{
+            while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+            }
+            ans=s2.top();
+        }
+        return ans;
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.empty()&&s2.empty();
     }
 };
 
