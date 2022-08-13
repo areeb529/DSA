d7c04020d383d3e4ac59d5ddc95624a19f63cc29
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-13 15:08:13 +0530

    Time: 16 ms (45.89%), Space: 17.2 MB (97.94%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,82 +12,30 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        // queue<pair<TreeNode*,long long>> pN;
-        // pN.push({root,0});
-        // long long maxWidth=0;
-        // while(!pN.empty()){
-        //     int n=pN.size();
-        //     long long minIdx=0,maxIdx=0;
-        //     long long start=0;
-        //     long long prev=-1;
-        //     for(int i=0;i<n;i++){
-        //         TreeNode*front=pN.front().first;
-        //         long long idx=pN.front().second;
-        //         cout<<"idx "<<idx<<" ";
-        //         pN.pop();
-        //         if(front->left){
-        //             if(prev==-1){
-        //                 start=0;
-        //                 prev=2*idx+1;
-        //             }
-        //             else{
-        //                 start=start+2*idx+1-prev;
-        //                 prev=2*idx+1;
-        //             }
-        //             cout<<"s p "<<start<<" "<<prev<<" ";
-        //             pN.push({front->left,start});
-        //         }
-        //         if(front->right){
-        //             if(prev==-1){
-        //                 start=0;
-        //                 prev=2*idx+2;
-        //             }
-        //             else{
-        //                 start=start+2*idx+2-prev;
-        //                 prev=2*idx+2;
-        //             }
-        //             cout<<"s p "<<start<<" "<<prev<<" ";
-        //             pN.push({front->left,start});
-        //         }
-        //         if(i==0){
-        //             minIdx=idx;
-        //         }
-        //         if(i==n-1){
-        //             maxIdx=idx;
-        //         }
-        //         cout<<endl;
-        //     }
-        //     cout<<endl;
-        //     maxWidth=max(maxWidth,maxIdx-minIdx+1);
-        //     cout<<"width"<<" "<<maxIdx-minIdx+1<<endl;
-        // }
-        // return maxWidth;
         queue<pair<TreeNode*,long long>> pN;
         pN.push({root,0});
-        long long maxWidth=0;
+        int maxWidth=1;
         while(!pN.empty()){
             int n=pN.size();
+            int width=0;
             long long minIdx=0,maxIdx=0;
             for(int i=0;i<n;i++){
-                TreeNode*front=pN.front().first;
-                long long idx=pN.front().second;
+                pair<TreeNode*,long long> front=pN.front();
                 pN.pop();
-                if(i==0){
-                    minIdx=idx;
-                }
-                if(i==n-1){
-                    maxIdx=idx;
-                }
+                TreeNode* node=front.first;
+                long long idx=front.second;
+                if(i==0)minIdx=idx;
+                if(i==n-1)maxIdx=idx;
                 idx=idx-minIdx;
-                if(front->left){
-                    pN.push({front->left,2*idx+1});
+                if(node->left){
+                    pN.push({node->left,2*idx+1});
                 }
-                if(front->right){
-                    pN.push({front->right,2*idx+2});
+                if(node->right){
+                    pN.push({node->right,2*idx+2});
                 }
-                
             }
-            maxWidth=max(maxWidth,maxIdx-minIdx+1);
+            width=maxIdx-minIdx+1;
+            maxWidth=max(maxWidth,width);
         }
         return maxWidth;
     }
