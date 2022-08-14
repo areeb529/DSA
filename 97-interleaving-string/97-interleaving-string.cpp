854b9c7da5c9c2fdc561eb1f9fec083d28818ba3
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-14 10:17:31 +0530

    Time: 4 ms (70.96%), Space: 7.2 MB (44.10%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    bool isInterleave(string s1, string s2, string s3) {
-        int m=s1.size(),n=s2.size();
-        if(m+n!=s3.size())return false;
-        vector<int> dp(n+1,0);
-        for(int i=m;i>=0;i--){
-            
-            for(int j=n;j>=0;j--){
-                int k=i+j;
-                if(k==s3.size()){
-                    dp[j]=1;
-                }
-                else if(i<m&&s1[i]==s3[k]&&j<n&&s2[j]==s3[k]){
-                    dp[j]=dp[j]||dp[j+1];
-                }
-                else if(i<m&&s1[i]==s3[k]){
-                    dp[j]=dp[j];
-                }
-                else if(j<n&&s2[j]==s3[k]){
-                    dp[j]=dp[j+1];
-                }
-                else{
-                    dp[j]=0;
-                }
-            }
+    bool helper(string &s1,int i,string &s2,int j,string &s3,int k,vector<vector<int>> &dp){
+        if(k==s3.size()){
+            if(i==s1.size()&&j==s2.size())return true;
+            else return false;
         }
-        return dp[0];
+        if(i==s1.size()&&j==s2.size()){
+            return false;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        bool op1=false,op2=false;
+        if(i<s1.size()&&s1[i]==s3[k]){
+            op1=helper(s1,i+1,s2,j,s3,k+1,dp);
+        }
+        if(j<s2.size()&&s2[j]==s3[k]){
+            op2=helper(s1,i,s2,j+1,s3,k+1,dp);
+        }
+        return dp[i][j]=op1||op2;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int m=s1.size(),n=s2.size(),k=s3.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+       return helper(s1,0,s2,0,s3,0,dp);
     }
 };
