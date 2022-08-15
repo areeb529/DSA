96eb1c776244a7b984252a4c6fab8b5f890d726f
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-15 14:22:17 +0530

    Time: 3 ms (97.52%), Space: 7.9 MB (72.21%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int getSetBits(int num){
-        int cnt=0;
-        while(num){
-            if(num&1)cnt++;
-            num=num>>1;
-        }
-        return cnt;
-    }
+    // int getSetBits(int num){
+    //     int cnt=0;
+    //     while(num){
+    //         if(num&1)cnt++;
+    //         num=num>>1;
+    //     }
+    //     return cnt;
+    // }
     vector<int> countBits(int n) {
-        vector<int> dp(n+1);
-        for(int i=0;i<=n;i++){
-            dp[i]=getSetBits(i);
+        // vector<int> dp(n+1);
+        // for(int i=0;i<=n;i++){
+        //     dp[i]=getSetBits(i);
+        // }
+        // return dp;
+        vector<int> dp(n+1,1);
+        dp[0]=0;
+        if(n<=2)return dp;
+        for(int i=3;i<=n;i++){
+            if(i&1){
+                dp[i]=dp[i>>1]+1;
+            }
+            else{
+                dp[i]=dp[i>>1];
+            }
         }
         return dp;
     }
