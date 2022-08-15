11a9868e625b4ae6726895491e3fc15e8671e4d0
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-15 12:20:15 +0530

    Time: 101 ms (92.47%), Space: 14 MB (69.41%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
+    unordered_set<string> st;
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int n=wordList.size();
-        unordered_set<string> st;
-        for(int i=0;i<n;i++){
-            st.insert(wordList[i]);
+        for(auto &i:wordList){
+            st.insert(i);
         }
-        if(st.count(endWord)==0)return 0;
-        int ladder=0;
         queue<string> pN;
         pN.push(beginWord);
+        int cnt=1;
+        bool flag=false;
         while(!pN.empty()){
-            int size=pN.size();
-            ladder++;
-            while(size--){
+            int n=pN.size();
+            for(int i=0;i<n;i++){
                 string word=pN.front();
                 if(word==endWord){
-                    return ladder;
+                    flag=true;
+                    break;
                 }
                 pN.pop();
-                st.erase(word);
-                for(int j=0;j<word.size();j++){
-                    char c=word[j];
-                    for(int k=0;k<26;k++){
-                        word[j]='a'+k;
-                        if(st.count(word)==1){
-                            pN.push(word);
-                        }
+                for(int k=0;k<word.size();k++){
+                    char temp=word[k];
+                    for(char ch='a';ch<='z';ch++){
+                        if(ch==temp)continue;
+                        word[k]=ch;
+                        if(st.count(word)==0)continue;
+                        pN.push(word);
+                        st.erase(word);
                     }
-                    word[j]=c;
+                    word[k]=temp;
                 }
             }
+            if(flag)break;
+            cnt++;
         }
-        return 0;
-        
+        if(flag)return cnt;
+        else return 0;
     }
 };
