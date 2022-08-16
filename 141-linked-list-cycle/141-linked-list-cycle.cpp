41afc713ecefd765dca1b205cbef3c67cd6caf95
Mohammad Areeb <69076491+areeb529@users.noreply.github.com>
2022-08-16 11:58:54 +0530

    Time: 18 ms (45.82%), Space: 8 MB (81.43%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -11,12 +11,15 @@ public:
     bool hasCycle(ListNode *head) {
         if(head==NULL||head->next==NULL)return false;
         ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast&&fast->next){
+        ListNode*fast=head->next;
+        while(slow&&fast){
             slow=slow->next;
+            if(fast->next)
             fast=fast->next->next;
+            else return false;
             if(slow==fast)return true;
         }
         return false;
+        
     }
 };
